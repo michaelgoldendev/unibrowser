c17e527b4d94e3f9a4b512d4ed7315b48425ccc7
michaelgoldendev <michaelgolden0@gmail.com>
2019-10-06 14:37:27 +0200

    Update filterexample.py

--- src/filterexample.py
+++ src/filterexample.py
@@ -21,14 +21,23 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
 if __name__ == "__main__":
     data = np.genfromtxt('data.csv', delimiter=',')
     c1 = data[:,1]
+    o1 = np.copy(c1)
     
+    # bandpass filtering
     fs = 5000.0
     lowcut = 500.0
     highcut = 1250.0
     b, a = butter_bandpass(lowcut, highcut, fs, order=5)
-    y = lfilter(b, a, c1)
-    print(y)
+    o1 = lfilter(b, a, o1)
     
+    # notch filtering
+    a,b = iirnotch(50.0,30.0,fs)
+    o1 = lfilter(b, a, c1)
+    
+    fout = open("output.csv","w")
+    for (x,y) in zip(c1,o1):
+        fout.write("%s,%s\n" % (x,y))
+    fout.close()
    
     """
 
